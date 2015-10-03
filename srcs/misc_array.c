49d601b2449356f29db06aeddd5bdff29036de9d
Julien Balestra <julien.balestra@gmail.com>
2015-10-03 19:18:52 +0200

    Array tools

---
+++ srcs/misc_array.c
@@ -0,0 +1,28 @@
+#include <stdlib.h>
+
+int tab_len(char **tab)
+{
+    int i;
+
+    i = 0;
+    if (tab)
+    {
+        while (tab[i])
+        {
+            i++;
+        }
+    }
+    return (i);
+}
+
+void tab_free(char **tab)
+{
+    int i;
+
+    i = 0;
+    while (tab[i])
+    {
+        free(tab[i]);
+        i++;
+    }
+}
