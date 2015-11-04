c9729355e1bcfb50d1117e3713227c515a5986df
Julien Balestra <julien.balestra@gmail.com>
2015-11-04 23:00:42 +0100

    Comment the prototypes

--- includes/sh1.h
+++ includes/sh1.h
@@ -6,21 +6,30 @@
 # define PROMPT "minishell> "
 # define PROMPT_SIZE 11
 
+// misc_string
 void str_clear(char *str);
 void buf_init(char *buf, int len);
 
+// misc_array
 int tab_len(char **tab);
 void tab_free(char **tab);
 
+// input_format
 char *ft_remove_endline(char *str);
 int is_only_endline(char *buf);
 int is_only_spaces(char *buf);
 
-char **environ(void);
-
+// misc_prompt
 void display_prompt(void);
 
-//main
+// main
 int pass_str_to_exec(const char **str);
+
+// data model
+typedef struct			s_sh
+{
+    int					ret;
+    char                **env;
+}						t_sh;
 
 #endif
