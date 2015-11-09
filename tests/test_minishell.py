e593222e98bb0104ee59d4ce78d899e262781c9b
Julien Balestra <julien.balestra@gmail.com>
2015-11-09 19:12:02 +0100

    Manage if valgrind is here or not

--- tests/test_minishell.py
+++ tests/test_minishell.py
@@ -1,7 +1,6 @@
 import unittest
 import subprocess
 import os
-
 from tools import valgrind_wrapper
 
 
@@ -11,20 +10,31 @@ class TestMinishell(unittest.TestCase):
 	testing_dir = "%s/tests/test_resources/" % context
 	minishell_prompt = "minishell> "
 	compiled = False
+	valgrind_binary = False
 
 	@classmethod
 	def setUpClass(cls):
 		os.chdir(cls.testing_dir)
 		# check if the project is well compiled
 		if os.path.isfile(cls.minishell) is False:
-			assert subprocess.call(["make", "-C", cls.context]) == 0
+			if subprocess.call(["make", "-C", cls.context, "-j"]) == 0:
+				pass
+			elif subprocess.call(["make", "-C", cls.context]) == 0:
+				os.write(2, "FAILED TO COMPILE WITH -j")
+			else:
+				raise RuntimeError("compilation failed in %s" % cls.context)
 		else:
 			cls.compiled = True
+		if subprocess.call(["valgrind", "--version"]) == 0:
+			cls.valgrind_binary = True
+
 
 	@classmethod
 	def tearDownClass(cls):
 		if cls.compiled is False:
 			subprocess.call(["make", "fclean", "-C", cls.context])
+		if cls.valgrind_binary is False:
+			raise AssertionError("valgrind --version ; test $? == 0")
 
 	def execute_my_shell(self, command):
 		"""
@@ -60,102 +70,125 @@ class TestMinishell(unittest.TestCase):
 		my_std = self.execute_my_shell(command)
 		self.assertEqual(real_std, my_std)
 
+	def valgrind(self, command):
+		if self.valgrind_binary is True:
+			self.assertFalse(valgrind_wrapper(self.minishell, leaks=True, command=command))
+			self.assertFalse(valgrind_wrapper(self.minishell, errors=True, command=command))
+
 	def test_00_full_bin_ls(self):
 		command = ["/bin/ls"]
 		self.compare_shells(command)
+		self.valgrind(command)
 
 	def test_01_full_bin_ls_opt(self):
 		command = ["/bin/ls", "-l"]
 		self.compare_shells(command)
+		self.valgrind(command)
 
 	def test_02_full_bin_ls_opt(self):
 		command = ["/bin/ls", "-l", "-a"]
 		self.compare_shells(command)
+		self.valgrind(command)
 
 	def test_03_full_bin_ls_opt(self):
 		command = ["/bin/ls", "-l", "-a", "-rt"]
 		self.compare_shells(command)
+		self.valgrind(command)
 
 	def test_04_empty(self):
 		command = [""]
 		self.compare_shells(command)
+		self.valgrind(command)
 
 	def test_05_empty(self):
 		command = [" "]
 		self.compare_shells(command)
+		self.valgrind(command)
 
 	def test_06_empty(self):
 		command = [" ", "  "]
 		self.compare_shells(command)
+		self.valgrind(command)
 
 	def test_07_with_empty_bin_ls(self):
 		command = [" ", "/bin/ls"]
 		self.compare_shells(command)
+		self.valgrind(command)
 
 	def test_08_with_empty_bin_ls(self):
 		command = ["   ", "/bin/ls", "    "]
 		self.compare_shells(command)
+		self.valgrind(command)
 
 	def test_09_with_empty_bin_ls(self):
 		command = ["   ", "/bin/ls", "-l", "    "]
 		self.compare_shells(command)
+		self.valgrind(command)
 
 	def test_10_bin_echo(self):
 		command = ["/bin/echo", "toto"]
 		self.compare_shells(command)
+		self.valgrind(command)
 
 	def test_11_bin_echo(self):
 		command = ["/bin/echo", "toto", "tata"]
 		self.compare_shells(command)
+		self.valgrind(command)
 
 	def test_12_bin_echo(self):
 		command = ["   ", "/bin/echo", "  toto  "]
 		self.compare_shells(command)
+		self.valgrind(command)
 
 	def test_13_bin_echo(self):
 		command = ["/bin/echo"]
 		self.compare_shells(command)
+		self.valgrind(command)
 
 	def test_14_echo(self):
 		command = ["echo"]
 		self.compare_shells(command)
+		self.valgrind(command)
 
 	def test_15_echo(self):
 		command = ["echo", "one"]
 		self.compare_shells(command)
+		self.valgrind(command)
 
 	def test_16_echo(self):
 		command = ["echo", "one", "two"]
 		self.compare_shells(command)
+		self.valgrind(command)
 
 	def test_17_ls(self):
 		command = ["ls"]
 		self.compare_shells(command)
+		self.valgrind(command)
 
 	def test_18_ls(self):
 		command = ["ls", "-l"]
 		self.compare_shells(command)
+		self.valgrind(command)
 
 	def test_19_ls(self):
 		command = ["ls", "-la"]
 		self.compare_shells(command)
+		self.valgrind(command)
 
 	def test_20_ls(self):
 		command = ["ls", "-la", "/"]
 		self.compare_shells(command)
+		self.valgrind(command)
 
 	def test_21_fake_command(self):
 		command = ["a_very_large_fake_binary_name"]
 		self.compare_shells(command)
+		self.valgrind(command)
 
 	def test_22_fake_command(self):
 		command = ["a_very_large_fake_binary_name", "-o", "/"]
 		self.compare_shells(command)
-
-	def test_A_leaks(self):
-		for command in [["ls", "-l"], ["/bin/ls"], ["   "], ["      \n"]]:
-			valgrind_wrapper(self.minishell, leaks=True, command=command)
-			valgrind_wrapper(self.minishell, errors=True, command=command)
+		self.valgrind(command)
 
 
 if __name__ == "__main__":
