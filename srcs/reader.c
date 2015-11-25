30eec702a3fb5b519ce9fd14b7be8c2e97165cc1
Julien Balestra <julien.balestra@gmail.com>
2015-11-25 19:47:41 +0100

    Manage signals SIGINT

--- srcs/reader.c
+++ srcs/reader.c
@@ -2,6 +2,7 @@
 #include <unistd.h>
 #include "libft.h"
 #include "minishell.h"
+#include <signal.h>
 
 #include "../includes/minishell.h"
 #include "../libft/includes/libft.h"
@@ -32,33 +33,38 @@ char *move_and_clean(char *buf)
 	return (left);
 }
 
+void signal_callback_handler(int sig_num)
+{
+	(void) sig_num;
+	ft_putchar('\n');
+	ft_putstr(PROMPT);
+}
+
 char *get_line(t_sh *shell)
 {
 	char *buf;
 	char *left;
 	ssize_t ret;
 
-	if ((buf = (char *)malloc(sizeof(char) * (BUFF_SIZE + 1))))
+	display_prompt(shell);
+	buf = ft_strnew(BUFF_SIZE);
+	left = NULL;
+	signal(SIGINT, signal_callback_handler);
+	while ((ret = read(0, buf, BUFF_SIZE)))
 	{
-		left = NULL;
-		display_prompt(shell);
-		buf_init(buf, BUFF_SIZE);
-		while ((ret = read(0, buf, BUFF_SIZE)))
-		{
-			buf[ret] = '\0';
-			if (left && buf[0])
-				buf = compile(left, buf);
-			if (is_real_line(buf))
-				return (buf);
-            else if (is_enter(buf))
-				again(buf, shell);
-			else
-				left = move_and_clean(buf);
-		}
-		if (left)
-			free(left);
-		free(buf);
-		shell->exit = 1;
+		buf[ret] = '\0';
+		if (left && buf[0])
+			buf = compile(left, buf);
+		if (is_real_line(buf))
+			return (buf);
+		else if (is_enter(buf))
+			again(buf, shell);
+		else
+			left = move_and_clean(buf);
 	}
+	if (left)
+		free(left);
+	free(buf);
+	shell->exit = 1;
 	return (NULL);
 }
