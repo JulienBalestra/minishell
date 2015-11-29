0deb1a35167e8385851b57f7fa30fdebd9f00236
Julien Balestra <julien.balestra@gmail.com>
2015-11-29 17:40:29 +0100

    Normalize

--- srcs/manage_builtins.c
+++ srcs/manage_builtins.c
@@ -3,7 +3,7 @@
 #include "../includes/minishell.h"
 #include "../libft/includes/libft.h"
 
-int manage_builtins(char **command, t_sh *shell)
+int part_one(char **command, t_sh *shell)
 {
 	if (ft_strcmp(command[0], "env") == 0)
 	{
@@ -25,7 +25,12 @@ int manage_builtins(char **command, t_sh *shell)
 		builtin_setenv(command, shell);
 		return (1);
 	}
-	else if (ft_strcmp(command[0], "getenv") == 0)
+	return (0);
+}
+
+int part_two(char **command, t_sh *shell)
+{
+	if (ft_strcmp(command[0], "getenv") == 0)
 	{
 		builtin_getenv(command, shell);
 		return (1);
@@ -35,5 +40,14 @@ int manage_builtins(char **command, t_sh *shell)
 		builtin_cd(command, shell);
 		return (1);
 	}
+	return (0);
+}
+
+int manage_builtins(char **command, t_sh *shell)
+{
+	if (part_one(command, shell))
+		return (1);
+	if (part_two(command, shell))
+		return (1);
 	return (0);
 }
