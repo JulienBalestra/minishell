596193fd3a784a122d37e964e433c493bf405fca
JulienBalestra <julien.balestra@gmail.com>
2015-12-03 15:33:08 +0100

    Norme

--- srcs/build_props.c
+++ srcs/build_props.c
@@ -1,18 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   build_props.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jubalest <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2015/12/03 15:31:14 by jubalest          #+#    #+#             */
+/*   Updated: 2015/12/03 15:31:18 by jubalest         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include <stdlib.h>
 #include "minishell.h"
 
 #include "../includes/minishell.h"
 #include "../libft/includes/libft.h"
 
-t_env *create_env_link(t_env *env, char *environ_entry)
+t_env		*create_env_link(t_env *env, char *environ_entry)
 {
-	t_env *link;
-	int len_until;
+	t_env	*link;
+	int		len_until;
 
-	if ((link = (t_env *) malloc(sizeof(t_env))))
+	if ((link = (t_env *)malloc(sizeof(t_env))))
 	{
 		len_until = len_to_char(environ_entry, '=');
-		link->name = ft_strndup(environ_entry, (size_t) len_until);
+		link->name = ft_strndup(environ_entry, (size_t)len_until);
 		environ_entry = &environ_entry[len_until + 1];
 		link->value = ft_strdup(environ_entry);
 		link->next = NULL;
@@ -28,9 +40,9 @@ t_env *create_env_link(t_env *env, char *environ_entry)
 	return (NULL);
 }
 
-t_env *build_env_list(char **environ)
+t_env		*build_env_list(char **environ)
 {
-	t_env *env;
+	t_env	*env;
 
 	env = NULL;
 	if (ft_str2len(environ) == 0)
@@ -49,11 +61,11 @@ t_env *build_env_list(char **environ)
 	return (get_start(env));
 }
 
-t_sh *create_shell_from(char **environ_tab)
+t_sh		*create_shell_from(char **environ_tab)
 {
-	t_sh *shell;
+	t_sh	*shell;
 
-	if ((shell = (t_sh *) malloc(sizeof(t_sh))))
+	if ((shell = (t_sh *)malloc(sizeof(t_sh))))
 	{
 		shell->env = build_env_list(environ_tab);
 		ensure_pwd(shell);
@@ -71,10 +83,10 @@ t_sh *create_shell_from(char **environ_tab)
 	return (NULL);
 }
 
-t_sh *create_shell_props(void)
+t_sh		*create_shell_props(void)
 {
-	t_sh *shell;
-	extern char **environ;
+	t_sh		*shell;
+	extern char	**environ;
 
 	if ((shell = create_shell_from(environ)))
 	{
