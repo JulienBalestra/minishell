23c31c6a992e69b4164f40604115535ef9e9090b
JulienBalestra <julien.balestra@gmail.com>
2015-12-03 15:40:50 +0100

    Norme

--- srcs/builtin_setenv.c
+++ srcs/builtin_setenv.c
@@ -1,14 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   builtin_setenv.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jubalest <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2015/12/03 15:39:05 by jubalest          #+#    #+#             */
+/*   Updated: 2015/12/03 15:39:07 by jubalest         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include <stdlib.h>
 #include "minishell.h"
 
 #include "../includes/minishell.h"
 #include "../libft/includes/libft.h"
 
-void add_new_env(t_env *env, char *name, char *value)
+void		add_new_env(t_env *env, char *name, char *value)
 {
-	t_env *new;
+	t_env	*new;
 
-	if ((new = (t_env *) malloc(sizeof(t_env))))
+	if ((new = (t_env *)malloc(sizeof(t_env))))
 	{
 		new->name = ft_strdup(name);
 		new->value = ft_strdup(value);
@@ -18,7 +30,7 @@ void add_new_env(t_env *env, char *name, char *value)
 	}
 }
 
-void modify_env_value(t_env *browse, char *value)
+void		modify_env_value(t_env *browse, char *value)
 {
 	if (browse && value)
 	{
@@ -27,9 +39,9 @@ void modify_env_value(t_env *browse, char *value)
 	}
 }
 
-void ft_setenv(char *name, char *value, t_sh *shell)
+void		ft_setenv(char *name, char *value, t_sh *shell)
 {
-	t_env *browse;
+	t_env	*browse;
 
 	browse = shell->env;
 	while (browse)
@@ -38,29 +50,29 @@ void ft_setenv(char *name, char *value, t_sh *shell)
 		{
 			modify_env_value(browse, value);
 			shell->l_env = override_last_environ(shell);
-			return;
+			return ;
 		}
 		if (browse->next)
 			browse = browse->next;
 		else
-			break;
+			break ;
 	}
 	add_new_env(browse, name, value);
 	shell->l_env = override_last_environ(shell);
 }
 
-void builtin_setenv(char **command, t_sh *shell)
+void		builtin_setenv(char **command, t_sh *shell)
 {
 	if (ft_str2len(command) == 1)
 	{
 		builtin_env(command, shell);
-		return;
+		return ;
 	}
 	else if (ft_str2len(command) > 3)
 	{
 		ft_putendl_fd("setenv: too many arguments", 2);
 		shell->l_ret = 2;
-		return;
+		return ;
 	}
 	ft_setenv(command[1], command[2], shell);
 }
