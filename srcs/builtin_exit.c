1b452dc50fe9183ab6c58a85abe938510cc0dbf1
JulienBalestra <julien.balestra@gmail.com>
2015-12-03 15:38:10 +0100

    Norme

--- srcs/builtin_exit.c
+++ srcs/builtin_exit.c
@@ -1,11 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   builtin_exit.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jubalest <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2015/12/03 15:37:41 by jubalest          #+#    #+#             */
+/*   Updated: 2015/12/03 15:37:43 by jubalest         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "minishell.h"
 
 #include "../includes/minishell.h"
 #include "../libft/includes/libft.h"
 
-void process_return_code(char **command, t_sh *shell)
+void	process_return_code(char **command, t_sh *shell)
 {
-	int ret;
+	int	ret;
 
 	ret = cust_atoi(command[1]);
 	if (ret == ft_atoi(command[1]))
@@ -25,7 +37,7 @@ void process_return_code(char **command, t_sh *shell)
 	}
 }
 
-void builtin_exit(char **command, t_sh *shell)
+void	builtin_exit(char **command, t_sh *shell)
 {
 	if (ft_str2len(command) > 1)
 	{
