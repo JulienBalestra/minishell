bdf0df8a69373e4b5a95564903c0daa0be7a951f
JulienBalestra <julien.balestra@gmail.com>
2015-12-03 15:47:51 +0100

    Norme

--- srcs/cd_goto.c
+++ srcs/cd_goto.c
@@ -1,9 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   cd_goto.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jubalest <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2015/12/03 15:41:14 by jubalest          #+#    #+#             */
+/*   Updated: 2015/12/03 15:41:15 by jubalest         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include <stdlib.h>
 #include "minishell.h"
 #include "../includes/minishell.h"
 #include "../libft/includes/libft.h"
 
-void go_to_home_directory(t_sh *shell)
+void	go_to_home_directory(t_sh *shell)
 {
 	if (get_env_value("HOME", shell->env))
 	{
@@ -16,16 +28,16 @@ void go_to_home_directory(t_sh *shell)
 	}
 }
 
-void go_to_old_pwd(t_sh *shell, int p)
+void	go_to_old_pwd(t_sh *shell, int p)
 {
-	char *old_pwd;
+	char	*old_pwd;
 
 	if (get_env_value("OLDPWD", shell->env))
 	{
 		if ((old_pwd = ft_strdup(get_env_value("OLDPWD", shell->env))))
 		{
 			change_dir(old_pwd, shell, p);
-			if (shell->l_ret == 0 && ft_strcmp(old_pwd, get_env_value("PWD", shell->env)) == 0)
+			if (shell->l_ret == 0)
 			{
 				ft_putendl(old_pwd);
 			}
